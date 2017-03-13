ad0f3659ba848896d5c33c101f1c67202818db34
Infokora <yanriy.slotvinskiy@gmail.com>
2017-03-13 13:25:29 +0200

    update

--- js/script.js
+++ js/script.js
@@ -1,7 +1,17 @@
+$(function () {
+    var $menu = $('li a');
+    $menu.on('click', function () {
+        console.log($(this).parents('ul, menu').find('a'));
+        $(this).parents('ul, menu').find('a').removeClass('active');
+        $(this).addClass('active');
+    });
 
-var $menu = $('li a');
-$menu.on('click', function(){
-    console.log($(this).parents('ul, menu').find('a'));
-    $(this).parents('ul, menu').find('a').removeClass('active');
-    $(this).addClass('active');
-});
+    $(window).scroll(function () {
+        console.log('aa');
+        if ($(this).scrollTop() > 0) {
+            $('nav').addClass('menu-fix');
+        } else {
+            $('nav').removeClass('menu-fix');
+        }
+    });
+})

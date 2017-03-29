c01b523d43142e7595931c3630abf1081d748a3f
Infokora <yanriy.slotvinskiy@gmail.com>
2017-03-29 17:05:47 +0300

    create 100% home page

--- js/script.js
+++ js/script.js
@@ -14,4 +14,40 @@ $(function () {
             $('nav').removeClass('menu-fix');
         }
     });
+
+    var $paginators = $('.paginators .paginator-list span'),
+        $slides = $('.headSlider li');
+
+    function goSlide() {
+        let $next = $('.headSlider .active').next(),
+            $pagAct = $('.paginator-list .active');
+        $slides.removeClass('active');
+        $paginators.removeClass('active');
+        if ($next.length) {
+            $next.addClass('active');
+            $pagAct.next().addClass('active');
+        } else {
+            $slides.eq(0).addClass('active');
+            $('.paginator-list span').eq(0).addClass('active');
+        }
+
+    }
+
+    var autoSlideGo = setInterval(goSlide, 5000);
+    $('.all-wrap').hover(function () {
+        clearInterval(autoSlideGo);
+    }, function () {
+        autoSlideGo = setInterval(goSlide, 5000);
+    });
+
+
+
+    $paginators.on('click', function () {
+        let id = $(this).index();
+
+        $paginators.removeClass('active');
+        $(this).addClass('active');
+        $('.headSlider li').removeClass('active');
+        $('.headSlider li').eq(id).addClass('active');
+    });
 })
